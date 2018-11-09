9fd929c60fe761bc7d06bfc85d524841123fde03
Sami Kukkonen <sami@samikukkonen.fi>
2018-11-09 14:47:31 +0200

    Exercise 1

--- src/main/java/ohtu/Main.java
+++ src/main/java/ohtu/Main.java
@@ -1,7 +1,35 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import org.apache.http.client.fluent.Request;
+
+import static java.util.Arrays.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        String studentId;
+        if (args.length > 0) {
+            studentId = args[0];
+        } else {
+            studentId = "012345678";
+        }
+        System.out.println(String.format("Opiskelijanumero: %s", studentId));
+
+        var url = "https://studies.cs.helsinki.fi/courses/students/" + studentId + "/submissions";
+        var bodyText = Request.Get(url).execute().returnContent().asString();
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+
+        for (Submission submission : subs) {
+            System.out.println("  " + submission);
+        }
+
+        System.out.println(String.format(
+            "Yhteensä: %d tehtävää, %d tuntia",
+            stream(subs).mapToInt(x -> x.getExercises().size()).sum(),
+            stream(subs).mapToInt(x -> x.getHours()).sum())
+        );
     }
 }
--- src/main/java/ohtu/Submission.java
+++ src/main/java/ohtu/Submission.java
@@ -1,7 +1,25 @@
 package ohtu;
 
+import com.google.common.collect.Streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class Submission {
     private int week;
+    private int hours;
+    private List<Integer> exercises;
+    private String course;
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
 
     public void setWeek(int week) {
         this.week = week;
@@ -11,9 +29,28 @@ public class Submission {
         return week;
     }
 
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public List<Integer> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<Integer> exercises) {
+        this.exercises = exercises;
+    }
+
     @Override
     public String toString() {
-        return ""+week;
+        return String.format("%s, viikko %d, tehtyjä tehtäviä yhteensä %d, aikaa kului %dh, tehdyt tehtävät: %s", course, week, exercises.size(), hours, String.join(", ", asStrings(exercises)));
     }
-    
+
+    private<A> Iterable<String> asStrings(Iterable<A> xs) {
+        return Streams.stream(xs).map(Objects::toString).collect(Collectors.toList());
+    }
 }
