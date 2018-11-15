e675017f22c6b2d636fa862f9e552b5ad15b38fa
Sami Kukkonen <sami@samikukkonen.fi>
2018-11-15 17:26:40 +0200

    Exercise 10

--- WebLogin/src/main/java/ohtu/Tester.java
+++ WebLogin/src/main/java/ohtu/Tester.java
@@ -13,12 +13,14 @@ public class Tester {
         driver.get("http://localhost:4567");
         
         sleep(2);
-        
+
+        // Go to login page
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
 
         sleep(2);
 
+        // Perform logging in with correct username and password
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
@@ -29,7 +31,38 @@ public class Tester {
         element.submit();
 
         sleep(3);
-        
+
+        // Go to login page
+        driver.get("http://localhost:4567");
+        sleep(2);
+        driver.findElement(By.linkText("login")).click();
+        sleep(2);
+
+        // Perform logging in with invalid username
+        driver.findElement(By.name("username")).sendKeys("sami");
+        driver.findElement(By.name("password")).sendKeys("isam");
+        sleep(2);
+        driver.findElement(By.name("login")).submit();
+        sleep(3);
+
+        // Perform creating a new user
+        driver.get("http://localhost:4567");
+        sleep(2);
+        driver.findElement(By.linkText("register new user")).click();
+        sleep(2);
+        driver.findElement(By.name("username")).sendKeys("sami");
+        driver.findElement(By.name("password")).sendKeys("isam");
+        driver.findElement(By.name("passwordConfirmation")).sendKeys("isam");
+        sleep(2);
+        driver.findElement(By.name("signup")).submit();
+        sleep(3);
+
+        // Log out
+        driver.findElement(By.linkText("continue to application mainpage")).click();
+        sleep(2);
+        driver.findElement(By.linkText("logout")).click();
+        sleep(3);
+
         driver.quit();
     }
     
@@ -38,4 +71,5 @@ public class Tester {
             Thread.sleep(n*1000);
         } catch(Exception e){}
     }
+
 }
